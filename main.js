d3d9a6e7a89ec69407ebbf7e7df7e312eb1bb404
Rpg87 <sra.picadillo@gmail.com>
2023-05-30 15:34:15 +0200

    add more js

--- main.js
+++ main.js
@@ -4,14 +4,15 @@
 
 const playerRed = 'R';
 const playerYellow = 'Y';
-const currentPlayer = playerRed;
-const gameOver = false;
+let currentPlayer = playerRed;
+let gameOver = false;
 
 let board;
-let currColumns;
+
 
 const rows = 6;
 const columns = 7;
+let currColumns = [];
 
 window.onload = function () {
     setGame();
@@ -25,12 +26,13 @@ function setGame() {
         let row = [];
         for (let c = 0; c < columns; c++) {
             //JS
-            row.push(' ')
+            row.push(' ');
 
             //HTML
             let tile = document.createElement('div');
-            tile.id = r.toString() + - + c.toString();
+            tile.id = r.toString() + '-' + c.toString();
             tile.classList.add('tile');
+            console.log(this);
             tile.addEventListener('click', setPiece);
             document.getElementById('board').append(tile);
             // Con estas líneas estamos creando un div, le añadimos ID ejemplo 0-0 (c=0 r=0), le añadimos una clase y por último lo añadimos a nuestro board gracias a append
@@ -47,7 +49,7 @@ function setPiece() {
     let coords = this.id.split('-'); //'0-0' -> ['0', '0']
     let r = parseInt(coords[0]);
     let c = parseInt(coords[1]);
-    r = currColumns
+    // r = currColumns
 
     r = currColumns[c];
     if (r < 0) {
@@ -67,4 +69,65 @@ function setPiece() {
     currColumns[c] = r; //update the array
     checkWinner();
 
+}
+
+function checkWinner() {
+    //horizontally
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r][c + 1]
+                    && board[r][c + 1] == board[r][c + 2] && board[r][c + 2] == board[r][c + 3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+    // vertically
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 3; r++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r + 1][c] && board[r + 1][c] == board[r + 2][c] && board[r + 2][c] == board[r + 3][c]) {
+                    setWinner(r, c);
+                    return;
+                }
+
+            }
+
+        }
+    }
+    //diagonally
+    for (let r = 0; r < rows - 3; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r + 1][c + 1] && board[r + 1][c + 1] == board[r + 2][c + 2] && board[r + 2][c + 2] == board[r + 3][c + 3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+    //up diagonally
+    for (let r = 3; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] != ' ') {
+                if (board[r][c] == board[r - 1][c + 1] && board[r - 1][c + 1] == board[r - 2][c + 2] && board[r - 2][c + 2] == board[r - 3][c + 3]) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+}
+
+function setWinner(r, c) {
+    let winner = document.getElementById('winner');
+    if (board[r][c] == playerRed) {
+        winner.innerHTML = 'Red Wins';
+    } else {
+        winner.innerHTML = 'Yellow wins'
+    }
+    gameOver = true;
 }
